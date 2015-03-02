a39915ccd1432f1d2abddcf54ab7092630d13f9c
Alan Files <alangfiles@gmail.com>
2015-03-02 15:14:19 -0500

    adding first test, and romanNumeralize function

--- romanNumeralizer.js
+++ romanNumeralizer.js
@@ -1,1 +1,21 @@
 ﻿runTests();
+
+function runTests(){
+    try{
+      testThatArabicNumeralOneIsRomanI();
+    }
+    catch(e){ 
+      console.log(e);
+    }
+    console.log("All Tests Passed");
+}
+
+function testThatArabicNumeralOneIsRomanI(){
+    if(romanNumeralize(1) != "I"){
+        throw new Error ("Arabic 1 doesn't return Roman I");
+    }
+}
+
+function romanNumeralize(arabicNumeral){
+    return "I";
+}
