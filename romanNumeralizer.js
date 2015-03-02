8219b605c35103f3a59d6721781b093a955a9902
Alan Files <alangfiles@gmail.com>
2015-03-02 15:16:47 -0500

    works for digits 1 - 3

--- romanNumeralizer.js
+++ romanNumeralizer.js
@@ -15,7 +15,18 @@ function testThatArabicNumeralOneIsRomanI(){
         throw new Error ("Arabic 1 doesn't return Roman I");
     }
 }
+function testThatArabicNumeralThreeIsRomanIII(){
+    if(romanNumeralize(3) != "III"){
+        throw new Error ("Arabic 3 doesn't return Roman III");
+    }
+}
+
 
 function romanNumeralize(arabicNumeral){
-    return "I";
+    if(arabicNumeral == 0){
+        return "";
+    }
+    else if(arabicNumeral >= 1){
+        return "I" + romanNumeralize(arabicNumeral-1);
+    }
 }
