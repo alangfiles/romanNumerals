a79b1cfb7ff0f96dd5a56d609b621702cf781669
Alan Files <alangfiles@gmail.com>
2015-03-02 15:20:39 -0500

    adding first subtraction numerals

--- romanNumeralizer.js
+++ romanNumeralizer.js
@@ -3,11 +3,14 @@
 function runTests(){
     try{
       testThatArabicNumeralOneIsRomanI();
+      testThatArabicNumeralThreeIsRomanIII();
+      testThatArabicNumeralSubtraction();
+      console.log("All Tests Passed");
     }
     catch(e){ 
       console.log(e);
     }
-    console.log("All Tests Passed");
+    
 }
 
 function testThatArabicNumeralOneIsRomanI(){
@@ -20,11 +23,27 @@ function testThatArabicNumeralThreeIsRomanIII(){
         throw new Error ("Arabic 3 doesn't return Roman III");
     }
 }
-
+function testThatArabicNumeralSubtraction(){
+    if(romanNumeralize(4) != "IV"){
+        throw new Error ("Arabic 4 doesn't return Roman IV");
+    }
+    if(romanNumeralize(9) != "IX"){
+        throw new Error ("Arabic 9 doesn't return Roman IX");
+    }
+}
 
 function romanNumeralize(arabicNumeral){
     if(arabicNumeral == 0){
         return "";
+    }
+    else if(arabicNumeral >= 9){
+        return "IX" + romanNumeralize(arabicNumeral-9);
+    }
+    else if(arabicNumeral >= 5){
+        return "V" + romanNumeralize(arabicNumeral-5);
+    }
+    else if(arabicNumeral >= 4){
+        return "IV" + romanNumeralize(arabicNumeral-4);
     }
     else if(arabicNumeral >= 1){
         return "I" + romanNumeralize(arabicNumeral-1);
