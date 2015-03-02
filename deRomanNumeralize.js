e21513c1d49d937e77e3a8801a7c63bebb63aca9
Alan Files <alangfiles@gmail.com>
2015-03-02 15:43:48 -0500

    adding deNumeralizer, with very basic setup

---
+++ deRomanNumeralize.js
@@ -0,0 +1,22 @@
+﻿runTests();
+
+function runTests(){
+    try{
+      testRomanNumeralToArabic();
+      console.log("All Tests Passed");
+    }
+    catch(e){ 
+      console.log(e);
+    }
+    
+}
+
+function testRomanNumeralToArabic(){
+    if(deRomanNumeralize("I") != 1){
+        throw new Error ("Roman I doesn't return arabic 1");
+    }
+}
+
+function deRomanNumeralize(romanNumberString){
+    return 1;
+}
